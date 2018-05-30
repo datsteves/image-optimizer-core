f661c8970177350b786f34a8bad376cc333625ef
Steffen Meyer <steffen@freiraum.xyz>
2018-05-30 18:20:22 +0200

    test docker with travis

---
+++ build_opencv.sh
@@ -0,0 +1,10 @@
+
+wget https://github.com/Itseez/opencv/archive/2.4.13.zip
+unzip 2.4.13.zip
+ls
+mkdir -p opencv-2.4.13/release
+cd opencv-2.4.13/release
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D BUILD_PYTHON_SUPPORT=ON -D WITH_XINE=ON -D WITH_TBB=ON ..
+make && make install
+cd /
+rm -rf opencv-2.4.13
